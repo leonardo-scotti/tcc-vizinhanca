9fbb687236a9e62d016428cfdf5179e6a29decbf
brenex920 <macahdo.breno127@gmail.com>
2026-04-10 14:26:25 -0300

    Istalação ambiente mobile

---
+++ code/mobile/app/src/main/java/com/tcc_mobile/tcc_vizinhaca/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.tcc_mobile.tcc_vizinhaca
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tcc_mobile.tcc_vizinhaca.ui.theme.Tcc_vizinhacaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Tcc_vizinhacaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
